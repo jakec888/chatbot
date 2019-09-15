7adbfc21a6d07a7b9e930cdc29007cd36bce8bbc
jj888 <jaconjcondes@gmail.com>
2019-09-15 15:49:12 -0700

    defined models (seq2seq); encoder and decoder(attention layer)

--- README.md
+++ README.md
@@ -1,7 +1,17 @@
 # Chatbot
 
+### Goal
+
+to learn basic ml.
+
 ## Tech Stack
 
 - python 3.6.7
 - torch 1.2.0
 - numpy 1.17.2
+
+#### Notes
+
+Extract Data
+Transform Data
+Load Data
--- main.py
+++ main.py
@@ -27,97 +27,97 @@ corpus_name = "cornell movie-dialogs corpus"
 corpus = os.path.join("data", corpus_name)
 
 
-# def printLines(file, n=10):
-#     with open(file, 'rb') as datafile:
-#         lines = datafile.readlines()
-#     for line in lines[:n]:
-#         print(line)
+def printLines(file, n=10):
+    with open(file, 'rb') as datafile:
+        lines = datafile.readlines()
+    for line in lines[:n]:
+        print(line)
 
 
-# printLines(os.path.join(corpus, "movie_lines.txt"))
+printLines(os.path.join(corpus, "movie_lines.txt"))
 
 
-# # Splits each line of the file into a dictionary of fields
-# def loadLines(fileName, fields):
-#     lines = {}
-#     with open(fileName, 'r', encoding='iso-8859-1') as f:
-#         for line in f:
-#             values = line.split(" +++$+++ ")
-#             # Extract fields
-#             lineObj = {}
-#             for i, field in enumerate(fields):
-#                 lineObj[field] = values[i]
-#             lines[lineObj['lineID']] = lineObj
-#     return lines
+# Splits each line of the file into a dictionary of fields
+def loadLines(fileName, fields):
+    lines = {}
+    with open(fileName, 'r', encoding='iso-8859-1') as f:
+        for line in f:
+            values = line.split(" +++$+++ ")
+            # Extract fields
+            lineObj = {}
+            for i, field in enumerate(fields):
+                lineObj[field] = values[i]
+            lines[lineObj['lineID']] = lineObj
+    return lines
 
 
-# # Groups fields of lines from `loadLines` into conversations based on *movie_conversations.txt*
-# def loadConversations(fileName, lines, fields):
-#     conversations = []
-#     with open(fileName, 'r', encoding='iso-8859-1') as f:
-#         for line in f:
-#             values = line.split(" +++$+++ ")
-#             # Extract fields
-#             convObj = {}
-#             for i, field in enumerate(fields):
-#                 convObj[field] = values[i]
-#             # Convert string to list (convObj["utteranceIDs"] == "['L598485', 'L598486', ...]")
-#             utterance_id_pattern = re.compile('L[0-9]+')
-#             lineIds = utterance_id_pattern.findall(convObj["utteranceIDs"])
-#             # Reassemble lines
-#             convObj["lines"] = []
-#             for lineId in lineIds:
-#                 convObj["lines"].append(lines[lineId])
-#             conversations.append(convObj)
-#     return conversations
+# Groups fields of lines from `loadLines` into conversations based on *movie_conversations.txt*
+def loadConversations(fileName, lines, fields):
+    conversations = []
+    with open(fileName, 'r', encoding='iso-8859-1') as f:
+        for line in f:
+            values = line.split(" +++$+++ ")
+            # Extract fields
+            convObj = {}
+            for i, field in enumerate(fields):
+                convObj[field] = values[i]
+            # Convert string to list (convObj["utteranceIDs"] == "['L598485', 'L598486', ...]")
+            utterance_id_pattern = re.compile('L[0-9]+')
+            lineIds = utterance_id_pattern.findall(convObj["utteranceIDs"])
+            # Reassemble lines
+            convObj["lines"] = []
+            for lineId in lineIds:
+                convObj["lines"].append(lines[lineId])
+            conversations.append(convObj)
+    return conversations
 
 
-# # Extracts pairs of sentences from conversations
-# def extractSentencePairs(conversations):
-#     qa_pairs = []
-#     for conversation in conversations:
-#         # Iterate over all the lines of the conversation
-#         # We ignore the last line (no answer for it)
-#         for i in range(len(conversation["lines"]) - 1):
-#             inputLine = conversation["lines"][i]["text"].strip()
-#             targetLine = conversation["lines"][i+1]["text"].strip()
-#             # Filter wrong samples (if one of the lists is empty)
-#             if inputLine and targetLine:
-#                 qa_pairs.append([inputLine, targetLine])
-#     return qa_pairs
+# Extracts pairs of sentences from conversations
+def extractSentencePairs(conversations):
+    qa_pairs = []
+    for conversation in conversations:
+        # Iterate over all the lines of the conversation
+        # We ignore the last line (no answer for it)
+        for i in range(len(conversation["lines"]) - 1):
+            inputLine = conversation["lines"][i]["text"].strip()
+            targetLine = conversation["lines"][i + 1]["text"].strip()
+            # Filter wrong samples (if one of the lists is empty)
+            if inputLine and targetLine:
+                qa_pairs.append([inputLine, targetLine])
+    return qa_pairs
 
 
-# # Define path to new file
+# Define path to new file
 datafile = os.path.join(corpus, "formatted_movie_lines.txt")
 
-# delimiter = '\t'
-# # Unescape the delimiter
-# delimiter = str(codecs.decode(delimiter, "unicode_escape"))
+delimiter = '\t'
+# Unescape the delimiter
+delimiter = str(codecs.decode(delimiter, "unicode_escape"))
 
-# # Initialize lines dict, conversations list, and field ids
-# lines = {}
-# conversations = []
-# MOVIE_LINES_FIELDS = ["lineID", "characterID", "movieID", "character", "text"]
-# MOVIE_CONVERSATIONS_FIELDS = ["character1ID",
-#                               "character2ID", "movieID", "utteranceIDs"]
+# Initialize lines dict, conversations list, and field ids
+lines = {}
+conversations = []
+MOVIE_LINES_FIELDS = ["lineID", "characterID", "movieID", "character", "text"]
+MOVIE_CONVERSATIONS_FIELDS = ["character1ID",
+                              "character2ID", "movieID", "utteranceIDs"]
 
-# # Load lines and process conversations
-# print("\nProcessing corpus...")
-# lines = loadLines(os.path.join(corpus, "movie_lines.txt"), MOVIE_LINES_FIELDS)
-# print("\nLoading conversations...")
-# conversations = loadConversations(os.path.join(corpus, "movie_conversations.txt"),
-#                                   lines, MOVIE_CONVERSATIONS_FIELDS)
+# Load lines and process conversations
+print("\nProcessing corpus...")
+lines = loadLines(os.path.join(corpus, "movie_lines.txt"), MOVIE_LINES_FIELDS)
+print("\nLoading conversations...")
+conversations = loadConversations(os.path.join(corpus, "movie_conversations.txt"),
+                                  lines, MOVIE_CONVERSATIONS_FIELDS)
 
-# # Write new csv file
-# print("\nWriting newly formatted file...")
-# with open(datafile, 'w', encoding='utf-8') as outputfile:
-#     writer = csv.writer(outputfile, delimiter=delimiter, lineterminator='\n')
-#     for pair in extractSentencePairs(conversations):
-#         writer.writerow(pair)
+# Write new csv file
+print("\nWriting newly formatted file...")
+with open(datafile, 'w', encoding='utf-8') as outputfile:
+    writer = csv.writer(outputfile, delimiter=delimiter, lineterminator='\n')
+    for pair in extractSentencePairs(conversations):
+        writer.writerow(pair)
 
-# # Print a sample of lines
-# print("\nSample lines from file:")
-# printLines(datafile)
+# Print a sample of lines
+print("\nSample lines from file:")
+printLines(datafile)
 
 
 # Default word tokens
@@ -286,3 +286,187 @@ def trimRareWords(voc, pairs, MIN_COUNT):
 
 # Trim voc and pairs
 pairs = trimRareWords(voc, pairs, MIN_COUNT)
+
+
+def indexesFromSentence(voc, sentence):
+    return [voc.word2index[word] for word in sentence.split(' ')] + [EOS_token]
+
+
+def zeroPadding(l, fillvalue=PAD_token):
+    return list(itertools.zip_longest(*l, fillvalue=fillvalue))
+
+
+def binaryMatrix(l, value=PAD_token):
+    m = []
+    for i, seq in enumerate(l):
+        m.append([])
+        for token in seq:
+            if token == PAD_token:
+                m[i].append(0)
+            else:
+                m[i].append(1)
+    return m
+
+# Returns padded input sequence tensor and lengths
+
+
+def inputVar(l, voc):
+    indexes_batch = [indexesFromSentence(voc, sentence) for sentence in l]
+    lengths = torch.tensor([len(indexes) for indexes in indexes_batch])
+    padList = zeroPadding(indexes_batch)
+    padVar = torch.LongTensor(padList)
+    return padVar, lengths
+
+# Returns padded target sequence tensor, padding mask, and max target length
+
+
+def outputVar(l, voc):
+    indexes_batch = [indexesFromSentence(voc, sentence) for sentence in l]
+    max_target_len = max([len(indexes) for indexes in indexes_batch])
+    padList = zeroPadding(indexes_batch)
+    mask = binaryMatrix(padList)
+    mask = torch.ByteTensor(mask)
+    padVar = torch.LongTensor(padList)
+    return padVar, mask, max_target_len
+
+# Returns all items for a given batch of pairs
+
+
+def batch2TrainData(voc, pair_batch):
+    pair_batch.sort(key=lambda x: len(x[0].split(" ")), reverse=True)
+    input_batch, output_batch = [], []
+    for pair in pair_batch:
+        input_batch.append(pair[0])
+        output_batch.append(pair[1])
+    inp, lengths = inputVar(input_batch, voc)
+    output, mask, max_target_len = outputVar(output_batch, voc)
+    return inp, lengths, output, mask, max_target_len
+
+
+# Example for validation
+small_batch_size = 5
+batches = batch2TrainData(voc, [random.choice(pairs)
+                                for _ in range(small_batch_size)])
+input_variable, lengths, target_variable, mask, max_target_len = batches
+
+print("input_variable:", input_variable)
+print("lengths:", lengths)
+print("target_variable:", target_variable)
+print("mask:", mask)
+print("max_target_len:", max_target_len)
+
+
+# MODELS HERE S2S
+class EncoderRNN(nn.Module):
+    def __init__(self, hidden_size, embedding, n_layers=1, dropout=0):
+        super(EncoderRNN, self).__init__()
+        self.n_layers = n_layers
+        self.hidden_size = hidden_size
+        self.embedding = embedding
+
+        # Initialize GRU; the input_size and hidden_size params are both set to 'hidden_size'
+        #   because our input size is a word embedding with number of features == hidden_size
+        self.gru = nn.GRU(hidden_size, hidden_size, n_layers,
+                          dropout=(0 if n_layers == 1 else dropout), bidirectional=True)
+
+    def forward(self, input_seq, input_lengths, hidden=None):
+        # Convert word indexes to embeddings
+        embedded = self.embedding(input_seq)
+        # Pack padded batch of sequences for RNN module
+        packed = nn.utils.rnn.pack_padded_sequence(embedded, input_lengths)
+        # Forward pass through GRU
+        outputs, hidden = self.gru(packed, hidden)
+        # Unpack padding
+        outputs, _ = nn.utils.rnn.pad_packed_sequence(outputs)
+        # Sum bidirectional GRU outputs
+        outputs = outputs[:, :, :self.hidden_size] + \
+            outputs[:, :, self.hidden_size:]
+        # Return output and final hidden state
+        return outputs, hidden
+
+
+# Luong attention layer
+class Attn(nn.Module):
+    def __init__(self, method, hidden_size):
+        super(Attn, self).__init__()
+        self.method = method
+        if self.method not in ['dot', 'general', 'concat']:
+            raise ValueError(
+                self.method, "is not an appropriate attention method.")
+        self.hidden_size = hidden_size
+        if self.method == 'general':
+            self.attn = nn.Linear(self.hidden_size, hidden_size)
+        elif self.method == 'concat':
+            self.attn = nn.Linear(self.hidden_size * 2, hidden_size)
+            self.v = nn.Parameter(torch.FloatTensor(hidden_size))
+
+    def dot_score(self, hidden, encoder_output):
+        return torch.sum(hidden * encoder_output, dim=2)
+
+    def general_score(self, hidden, encoder_output):
+        energy = self.attn(encoder_output)
+        return torch.sum(hidden * energy, dim=2)
+
+    def concat_score(self, hidden, encoder_output):
+        energy = self.attn(torch.cat(
+            (hidden.expand(encoder_output.size(0), -1, -1), encoder_output), 2)).tanh()
+        return torch.sum(self.v * energy, dim=2)
+
+    def forward(self, hidden, encoder_outputs):
+        # Calculate the attention weights (energies) based on the given method
+        if self.method == 'general':
+            attn_energies = self.general_score(hidden, encoder_outputs)
+        elif self.method == 'concat':
+            attn_energies = self.concat_score(hidden, encoder_outputs)
+        elif self.method == 'dot':
+            attn_energies = self.dot_score(hidden, encoder_outputs)
+
+        # Transpose max_length and batch_size dimensions
+        attn_energies = attn_energies.t()
+
+        # Return the softmax normalized probability scores (with added dimension)
+        return F.softmax(attn_energies, dim=1).unsqueeze(1)
+
+
+class LuongAttnDecoderRNN(nn.Module):
+    def __init__(self, attn_model, embedding, hidden_size, output_size, n_layers=1, dropout=0.1):
+        super(LuongAttnDecoderRNN, self).__init__()
+
+        # Keep for reference
+        self.attn_model = attn_model
+        self.hidden_size = hidden_size
+        self.output_size = output_size
+        self.n_layers = n_layers
+        self.dropout = dropout
+
+        # Define layers
+        self.embedding = embedding
+        self.embedding_dropout = nn.Dropout(dropout)
+        self.gru = nn.GRU(hidden_size, hidden_size, n_layers,
+                          dropout=(0 if n_layers == 1 else dropout))
+        self.concat = nn.Linear(hidden_size * 2, hidden_size)
+        self.out = nn.Linear(hidden_size, output_size)
+
+        self.attn = Attn(attn_model, hidden_size)
+
+    def forward(self, input_step, last_hidden, encoder_outputs):
+        # Note: we run this one step (word) at a time
+        # Get embedding of current input word
+        embedded = self.embedding(input_step)
+        embedded = self.embedding_dropout(embedded)
+        # Forward through unidirectional GRU
+        rnn_output, hidden = self.gru(embedded, last_hidden)
+        # Calculate attention weights from the current GRU output
+        attn_weights = self.attn(rnn_output, encoder_outputs)
+        # Multiply attention weights to encoder outputs to get new "weighted sum" context vector
+        context = attn_weights.bmm(encoder_outputs.transpose(0, 1))
+        # Concatenate weighted context vector and GRU output using Luong eq. 5
+        rnn_output = rnn_output.squeeze(0)
+        context = context.squeeze(1)
+        concat_input = torch.cat((rnn_output, context), 1)
+        concat_output = torch.tanh(self.concat(concat_input))
+        # Predict next word using Luong eq. 6
+        output = self.out(concat_output)
+        output = F.softmax(output, dim=1)
+        # Return output and final hidden state
+        return output, hidden
